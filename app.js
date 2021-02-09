a05e7d0dc2b87b85d48e0bdf4410b86e22f0b8b6
Daniele Esposito <esp.dne@gmail.com>
2021-02-09 17:43:41 +0100

    post + get

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Product = require("./modules/Product")
+const Product = require("./modules/Product");
 require("dotenv").config(); // keep it like it if you do not have Heroku
 /* if (process.env.NODE_ENV == "development") { // to be deployed in Heroku
     require("dotenv").config();
@@ -16,7 +16,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Connect to MongoDB
-mongoose.connect(process.env.DB_CONNECTION, {
+mongoose
+  .connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -28,14 +29,35 @@ mongoose.connect(process.env.DB_CONNECTION, {
 
 let product = [];
 
-app.get("/", (req, res)=>{  
-    res.send("Hello world!"); 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
-app.get("/")
+// POST - create one product
+app.post("/product", async (req, res) => {
+  const product = new Product({
+    img: req.body.img,
+    name: req.body.name,
+    description: req.body.description,
+  });
+  try {
+    const savedProduct = await product.save();
+    res.json(savedProduct);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-// listen 
+// GET - List of Products
+
+app.get("/shop-list", async (req, res) => {
+  const shopList = await Product.find({}).exec();
+  if (!shopList) throw Error("No Items");
+  res.status(200).json(shopList);
+});
+
+// listen
 
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, ()=> console.log(`Server run on port ${PORT}`)); // npm run "start" https://www.youtube.com/watch?v=kV6MJ9W4whM for nodemon 
+app.listen(PORT, () => console.log(`Server run on port ${PORT}`)); // npm run "start" https://www.youtube.com/watch?v=kV6MJ9W4whM for nodemon
